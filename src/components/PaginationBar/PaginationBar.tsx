251e7c34adf78e0840cccaf1ac3e8fb18f345998
unknown <edimariobabau@gmail.com>
2021-12-15 23:47:39 -0300

    Feature: PaginationBar component

---
+++ src/components/PaginationBar/PaginationBar.tsx
@@ -0,0 +1,39 @@
+import Pagination from '@choc-ui/paginator';
+import { Dispatch, SetStateAction } from 'react';
+export type PaginationBarProps = {
+  page: number;
+  setPageSize: Dispatch<SetStateAction<number>>;
+  pageSize: number;
+  setPage: Dispatch<SetStateAction<number>>;
+  totalPages: number;
+  pageSizeOptions?: number[];
+};
+
+export const PaginationBar = ({
+  page,
+  setPageSize,
+  pageSize,
+  setPage,
+  totalPages,
+  pageSizeOptions = [15, 30, 50],
+  ...restProps
+}: PaginationBarProps) => {
+  return (
+    <Pagination
+      total={totalPages}
+      paginationProps={{ display: 'flex' }}
+      pageSize={pageSize}
+      pageSizeOptions={pageSizeOptions}
+      showSizeChanger
+      current={page}
+      pageNeighbours={1}
+      onShowSizeChange={(_, newPageSize) => {
+        setPageSize(Number(newPageSize));
+      }}
+      onChange={(actualPage: any) => {
+        setPage(actualPage);
+      }}
+      {...restProps}
+    />
+  );
+};
